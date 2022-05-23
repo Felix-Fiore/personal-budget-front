8219390948be4f4a6f40015b0337dca45bf2baa6
Felix <feelix.nqn@live.com>
2022-05-23 13:00:35 -0300

    minor changes on totalAmount

--- src/components/layout/TotalAmount.jsx
+++ src/components/layout/TotalAmount.jsx
@@ -2,8 +2,18 @@ import styles from './totalAmount.module.scss';
 
 export const TotalAmount = () => {
     return (
-        <div className={styles.amountContainer}>
-            <div className="totalCash">$3000</div>
+        <div className={styles.container}>
+            <div>
+                <h2>Welcome User!</h2>
+            </div>
+            <div className={styles.amountContainer}>
+                <div>
+                    <h3>This is your balance</h3>
+                </div>
+                <div className="totalCash">
+                    <p>$3000</p>
+                </div>
+            </div>
         </div>
     );
 };
