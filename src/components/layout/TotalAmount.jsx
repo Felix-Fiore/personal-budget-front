fef1ce58e1e4c0b1cdf6b54c81f3ff244e3ac763
Felix <feelix.nqn@live.com>
2022-05-23 11:40:43 -0300

    totalAmount added

---
+++ src/components/layout/TotalAmount.jsx
@@ -0,0 +1,9 @@
+import styles from './totalAmount.module.scss';
+
+export const TotalAmount = () => {
+    return (
+        <div className={styles.amountContainer}>
+            <div className="totalCash">$3000</div>
+        </div>
+    );
+};
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import { NavBar } from '../components/layout/NavBar.jsx';
+import { TotalAmount } from '../components/layout/TotalAmount.jsx';
 
 export const HomeScreen = () => {
     return (
         <>
             <NavBar />
+            <TotalAmount />
         </>
     );
 };
