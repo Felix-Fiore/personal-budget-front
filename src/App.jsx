85c37f49d229c52101e426c9bd18b6d98dd3827b
Felix <feelix.nqn@live.com>
2022-05-30 19:30:55 -0300

    react router and some minor changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss';
 import { HomeScreen } from './screens/HomeScreen';
+import { OperationsScreen } from './screens/OperationsScreen';
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/layout/NavBar';
+import { LoginScreen } from './screens/LoginScreen';
 
 function App() {
     return (
         <>
-            <HomeScreen />
+            <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/operations" element={<OperationsScreen />} />
+                <Route path="/login" element={<LoginScreen />} />
+            </Routes>
         </>
     );
 }
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,18 +1,39 @@
+import { Link, useNavigate, NavLink } from 'react-router-dom';
 import styles from './navBar.module.scss';
 
 export const NavBar = () => {
+    let navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate('/login');
+    };
+
     return (
         <div className={styles.navBar}>
             <ul>
                 <li>
-                    <span className="material-symbols-outlined">home</span>
-                    <a>Home</a>
+                    <NavLink
+                        className={({ isActive }) => {
+                            return isActive ? styles.active : '';
+                        }}
+                        to="/"
+                    >
+                        <span className="material-symbols-outlined">home</span>
+                        Home
+                    </NavLink>
                 </li>
                 <li>
-                    <span className="material-symbols-outlined">
-                        view_timeline
-                    </span>
-                    <a>Operations Panel</a>
+                    <NavLink
+                        className={({ isActive }) => {
+                            return isActive ? styles.active : '';
+                        }}
+                        to="/operations"
+                    >
+                        <span className="material-symbols-outlined">
+                            view_timeline
+                        </span>
+                        Operations Panel
+                    </NavLink>
                 </li>
             </ul>
             <ul className={styles.rightSide}>
@@ -22,7 +43,7 @@ export const NavBar = () => {
                     </div>
                 </li>
                 <li>
-                    <button>
+                    <button onClick={navigateToLogin}>
                         {' '}
                         <span className="material-symbols-outlined">
                             logout
