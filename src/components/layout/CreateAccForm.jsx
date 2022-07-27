0535734e25f6e88229de34620ea43014c640a601
Felix <feelix.nqn@live.com>
2022-07-27 15:46:11 -0300

    Create user slice done and implemented.

--- src/components/layout/CreateAccForm.jsx
+++ src/components/layout/CreateAccForm.jsx
@@ -1,23 +1,71 @@
 import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './createAccform.module.scss';
+import { useForm } from '../../hooks/useForm';
+import { useEffect } from 'react';
+import { createUser } from '../../store/slices/userSlice/userThunks';
+
+const CreateUserFormFields = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 export const CreateAccForm = () => {
-  let navigate = useNavigate();
+  const { name, email, password, onInputChange } =
+    useForm(CreateUserFormFields);
 
-  const navigateToHome = () => {
-    navigate('/');
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const { isLoggedIn } = useSelector((state) => state.user);
+
+  //navigate('/');
+  const handleCreateAcc = (e) => {
+    e.preventDefault();
+    console.log({ name, email, password });
+
+    dispatch(createUser(name, email, password));
   };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn]);
 
   return (
     <div className={styles.formContainer}>
       <form>
+        <label>Nombre</label>
+        <input
+          type="text"
+          className={styles.input}
+          name="name"
+          value={name}
+          onChange={onInputChange}
+        />
+
         <label>Email</label>
-        <input type="email" className={styles.input} />
+        <input
+          type="email"
+          className={styles.input}
+          name="email"
+          value={email}
+          onChange={onInputChange}
+        />
 
         <label>Contraseña</label>
-        <input type="password" className={styles.input} />
+        <input
+          type="password"
+          className={styles.input}
+          name="password"
+          value={password}
+          onChange={onInputChange}
+        />
 
-        <button className={styles.btn} onClick={navigateToHome}>
+        <button className={styles.btn} onClick={handleCreateAcc}>
           Crear Cuenta
         </button>
         <NavLink to={'/login'}>
--- src/store/slices/userSlice/userThunks.js
+++ src/store/slices/userSlice/userThunks.js
@@ -2,7 +2,7 @@ import { personalBudgetAPI } from '../../../api/personalBudgetAPI';
 import { userSlice } from './userSlice';
 import { toast } from 'react-hot-toast';
 
-export const userLogin = (email, password) => {
+const userLogin = (email, password) => {
   return async (dispatch) => {
     try {
       const response = await personalBudgetAPI.post('/users', {
@@ -25,3 +25,30 @@ export const userLogin = (email, password) => {
     }
   };
 };
+
+const createUser = (name, email, password) => {
+  return async (dispatch) => {
+    try {
+      const response = await personalBudgetAPI.post('/users/new', {
+        name,
+        email,
+        password,
+      });
+
+      dispatch(
+        userSlice.actions.setUser({
+          name: response.data.name,
+          token: response.data.token,
+        })
+      );
+    } catch (error) {
+      if (error.response.data.errors) {
+        error.response.data.errors.map(({ msg }) => {
+          toast.error(msg);
+        });
+      }
+    }
+  };
+};
+
+export { userLogin, createUser };
